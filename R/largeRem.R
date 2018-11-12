b4931b7dd270dbc1a80b2cbd9cee72896551f8c5
basilesimon <basile@basilesimon.fr>
2018-11-12 16:42:49 +0000

    Include refactor from @ltrgoddard

--- R/largeRem.R
+++ R/largeRem.R
@@ -14,28 +14,22 @@
 
 largeRem <- function(pcVec) {
 
-  # Convert the list passed to the function into a data frame,
-  df_tmp <- data.frame(pcVec)
-
-  try(if(sum(df_tmp$pcVec) != 100) stop("The sum of the items in your column isn't equal to 100"))
-
-  # Isolate the integers and the decimal points into separate columns
-  df_tmp <- df_tmp %>%
-    mutate(integers = round(pcVec)) %>%
-    mutate(decimalPoints =
-             as.numeric(unlist(lapply(strsplit(sprintf("%.2f", pcVec), '.', fixed = TRUE),"[[",2))))
+  if(sum(pcVec) != 100) stop("The sum of the items in your column isn't equal to 100")
 
   # If the sum of our rounds() is 99, we need to add one;
   # if greater than 100, we need to substract one
   # In case of equality (eg c(33.33, 33.33, 33.33)) we pick one at random with sample()
-  diffTo100 <- 100 - sum(df_tmp$integers)
+  diffTo100 <- 100 - sum(as.integer(pcVec))
   if (diffTo100 == 1) {
-    index <- as.numeric(sample(row.names(df_tmp %>% filter(decimalPoints == max(decimalPoints))),1))
-    df_tmp[index,"integers"] <- df_tmp[index,"integers"] + 1
-
+    index <- which.max(pcVec %% 1)
   } else if (diffTo100 == -1)  {
-    index <- as.numeric(sample(row.names(df_tmp %>% filter(decimalPoints == min(decimalPoints))),1))
-    df_tmp[index,"integers"] <- df_tmp[index,"integers"] - 1
+    index <- which.min(pcVec %% 1)
   }
-  return(df_tmp$integers)
+
+  if(length(unique(pcVec %% 1)) == 1) {
+    index <- sample(1:length(pcVec), 1)
+  }
+
+  pcVec[index] <- pcVec[index] + diffTo100
+  return(as.integer(pcVec))
 }
